ee7497e8efedc283b0356255ba19e9eede848574
Paulo Monteiro <paulomonteiro7.pm@gmail.com>
2017-05-04 18:59:00 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,22 @@
 module.exports = {
   root: true,
   parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: 'module'
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      "experimentalObjectRestSpread": true
+    }
   },
-  extends: 'eslint:recommended',
+  extends: [
+    'eslint:recommended',
+    'plugin:ember-suave/recommended'
+  ],
   env: {
-    browser: true
+    'browser': true,
+    "es6": true
   },
   rules: {
+    "camelcase": [0, {"properties": "never"}],
+    "no-console": ["error", { allow: ["warn", "error"] }]
   }
 };
