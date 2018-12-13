3032e67a0750150c064f829b5512e21c2e113c20
André Joaquim <andrej_21@hotmail.com>
2018-12-13 11:19:23 +0000

    Run codemods

--- addon/utils/countries.js
+++ addon/utils/countries.js
@@ -1,14 +1,11 @@
-import Ember from 'ember';
 import Countries from 'ember-cli-countries/enums/countries';
 import CallingCodes from 'ember-cli-countries/enums/calling-codes';
-
-const { Logger } = Ember;
 
 export function getCountryName(countryCode) {
   let name = Countries[countryCode];
 
   if (!name) {
-    Logger.error(`The country with code ${countryCode} was not found`);
+    console.error(`The country with code ${countryCode} was not found`);
 
     return null;
   }
@@ -20,7 +17,7 @@ export function getCountryShortCode(countryCode) {
   let country = Countries[countryCode];
 
   if (!country) {
-    Logger.error(`The country with code ${countryCode} was not found`);
+    console.error(`The country with code ${countryCode} was not found`);
 
     return null;
   }
@@ -32,7 +29,7 @@ export function getCountryCallingCode(countryCode) {
   let callingCode = CallingCodes[countryCode];
 
   if (!callingCode) {
-    Logger.error(`The country with code ${countryCode} was not found`);
+    console.error(`The country with code ${countryCode} was not found`);
 
     return null;
   }
--- tests/unit/helpers/country-name-test.js
+++ tests/unit/helpers/country-name-test.js
@@ -4,20 +4,20 @@ import { module, test } from 'qunit';
 const TEST_CODE = 'PT';
 const EXPECTED_NAME = 'Portugal';
 
-module('Unit | Helper | country name');
+module('Unit | Helper | country name', function() {
+  test('it works', function(assert) {
+    assert.expect(1);
 
-test('it works', function(assert) {
-  assert.expect(1);
+    let name = countryName(TEST_CODE);
 
-  let name = countryName(TEST_CODE);
+    assert.equal(name, EXPECTED_NAME);
+  });
 
-  assert.equal(name, EXPECTED_NAME);
-});
+  test('it fails', function(assert) {
+    assert.expect(1);
 
-test('it fails', function(assert) {
-  assert.expect(1);
+    let name = countryName();
 
-  let name = countryName();
-
-  assert.equal(name, null);
+    assert.equal(name, null);
+  });
 });
